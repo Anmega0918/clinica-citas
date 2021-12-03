b95eeed7e3cf7da7a4e0240d9a7032c55552fdba
Anmega09 <andresmerchan0918@gmail.com>
2021-12-02 22:45:18 -0500

    se sube el proyecto a heroku

--- citas/citas/settings.py
+++ citas/citas/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import django_heroku django_heroku.settings(locals())
 import os
 from pathlib import Path
 from datetime import timedelta
@@ -141,3 +142,4 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
